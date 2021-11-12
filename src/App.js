75fa624494ebef0e69bd8f9aaa4aeaa0ff3c544c
meashishpokhrel <meashishpokhrel@gmail.com>
2021-11-12 09:44:14 +0545

    Login Page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import React from 'react';
+import SignIn from './component/sign-in/sign-in.component';
+import Login from './component/sign-in/sign-in.component';
+class App extends React.Component {
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+        <Router>
+          <Routes>
+            <Route exact path="/" element = {<SignIn />} />
+            <Route exact path="/signin" element = {<SignIn />} />
+        </Routes>
+      </Router>
+
+      
     </div>
   );
+  }
 }
 
 export default App;
