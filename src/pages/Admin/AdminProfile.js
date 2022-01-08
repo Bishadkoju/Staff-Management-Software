f06788102bd0d3950621a8c4d607722f3badb15d
NirajanBekoju <nirajan.bekoju@gmail.com>
2022-01-08 18:32:02 +0545

    UI fixed

--- src/pages/Admin/AdminProfile.js
+++ src/pages/Admin/AdminProfile.js
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import AdminLayout from "../../HOC/AdminLayout";
 import AdminSideNavBar from "../../component/Bar/AdminSideNavBar";
 
 import ProfileInfo from "../../component/Dashboard/Profile/ProfileInfo";
 import ProfileModal from "../../component/Dashboard/Modal/ProfileModal";
 
+import axiosInstance from "../../HelperFunction/Axios";
+
 const AdminProfile = () => {
+  const [adminInfo, setAdminInfo] = useState([]);
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    const getAdminInfo = async () => {
+      await axiosInstance
+        .get(`/user/self/view`)
+        .then((res) => {
+          console.log("From admin")
+          console.log(res.data);
+          setName(res.data.first_name + " " + res.data.last_name);
+          setAdminInfo([
+            res.data.email,
+            res.data.address,
+            res.data.phone_number,
+            res.data.emergency_contact.full_name,
+            res.data.father_name,
+            res.data.mother_name,
+            res.data.marital_status,
+            res.data.educational_status,
+            res.data.store,
+            res.data.joined_date,
+            res.data.termination_date,
+          ]);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+    getAdminInfo();
+  }, []);
+
   return (
     <div className="body">
       <AdminLayout />
@@ -33,7 +67,7 @@ const AdminProfile = () => {
                 </div>
               </div>
             </div>
-            <ProfileInfo />
+            <ProfileInfo userInfo =  {adminInfo} name = {name}/>
             <ProfileModal />
           </div>
         </div>
